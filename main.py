fa8b0228495eda9a9d571cdd942520fb90b56260
Lizzy <lizzym236@gmail.com>
2023-01-08 23:51:51 -0800

    Bidding, passing, scoring meld done. Bug with retyping bid. Bug with sorting cards (10, 9, A, J, K, Q).

--- main.py
+++ main.py
@@ -2,28 +2,35 @@ import random
 
 class Card:
 
-    def __init__(self, suit, rank, order):
+    def __init__(self, suit, rank):
         self._suit = suit
         self._rank = rank
-        self._order = order #H=0, S=1, D=2, C=3
-
-    def getSortOrder(self):
-        return self._order
 
     def getCard(self):
         return (self._suit, self._rank)
+
+    def isEqual(self, card):
+        if self._suit == card._suit and self._rank == card._rank:
+            return True
+        else:
+            return False
+
+    def getSimpleCard(self):
+        return self._suit[0] + self._rank
 
 class Deck:
 
     def __init__(self):
         ranks = ["A", "10", "K", "Q", "J", "9"]
-        suits = ["hearts", "spades", "diamonds", "clubs"]
+        self._suits = ["hearts", "spades", "diamonds", "clubs"]
         self._deck = []
-        for suit in suits:
+        for suit in self._suits:
             for rank in ranks:
-                order = suits.index(suit)*10 + ranks.index(rank)
-                self._deck.append(Card(suit, rank, order))
-                self._deck.append(Card(suit, rank, order))
+                self._deck.append(Card(suit, rank))
+                self._deck.append(Card(suit, rank))
+
+    def getSuits(self):
+        return self._suits
 
     def shuffle(self):
         random.shuffle(self._deck)
@@ -39,19 +46,34 @@ class Player:
         self._name = name
         self._hand = []
         self._bid = ""
+        self._meld = 0
 
     def getName(self):
         return self._name
+
+    def getCardFromSimple(self, simpleCard):
+        for card in self._hand:
+            if card.getSimpleCard() == simpleCard:
+                return card
 
     def fillHand(self, card):
         self._hand.append(card)
         self.sortHand()
 
+    def removeCard(self, card):
+        self._hand.remove(card)
+
     def getHand(self):
         return self._hand
 
+    def getSimpleHand(self):
+        simpleHand = []
+        for card in self._hand:
+            simpleHand.append(card.getSimpleCard())
+        return simpleHand
+
     def sorting(self, card):
-        return card.getSortOrder()
+        return card.getSimpleCard()
 
     def sortHand(self):
         self._hand.sort(key=self.sorting)
@@ -86,11 +108,70 @@ class Player:
     def getBid(self):
         return self._bid
 
+    def scoreGroups(self, group, score, doubleScore):
+        tempHand = self.getSimpleHand()
+        if set(group).issubset(tempHand):
+            tempHand = set(tempHand) - set(group)
+            if set(group).issubset(tempHand):
+                self._meld += doubleScore
+            else:
+                self._meld += score
+
+    def scoreHand(self, trump):
+        acesAround = ["hA", "sA", "dA", "cA"]
+        kingsAround = ["hK", "sK", "dK", "cK"]
+        queensAround = ["hQ", "sQ", "dQ", "cQ"]
+        jacksAround = ["hJ", "sJ", "dJ", "cJ"]
+
+        run = [trump[0] + "A", trump[0] + "10", trump[0] + "K", trump[0] + "Q", trump[0] + "J"]
+        royalMarriage = [trump[0] + "K", trump[0] + "Q"]
+        royalHalfMarriageA = [trump[0] + "A", trump[0] + "10", trump[0] + "K", trump[0] + "K", trump[0] + "Q", trump[0] + "J"]
+        royalHalfMarriageB = [trump[0] + "A", trump[0] + "10", trump[0] + "K", trump[0] + "Q", trump[0] + "Q", trump[0] + "J"]
+        nine = [trump[0] + "9"]
+
+        pinochle = ["sQ", "dJ"]
+
+        ranks = ["A", "10", "K", "Q", "J", "9"]
+        suits = ["hearts", "spades", "diamonds", "clubs"]
+
+        self.scoreGroups(acesAround, 100, 1000)
+        self.scoreGroups(kingsAround, 80, 800)
+        self.scoreGroups(queensAround, 60, 600)
+        self.scoreGroups(jacksAround, 40, 400)
+
+        if set(run).issubset(self.getSimpleHand()):
+            self.scoreGroups(run, 150, 1500)
+            if set(royalHalfMarriageA).issubset(self.getSimpleHand()):
+                self._meld += 20
+            if set(royalHalfMarriageB).issubset(self.getSimpleHand()):
+                self._meld += 20
+        elif set(royalMarriage).issubset(self.getSimpleHand()):
+            self.scoreGroups(royalMarriage, 40, 80)
+
+        self.scoreGroups(nine, 10, 20)
+        self.scoreGroups(pinochle, 40, 300)
+
+        for suit in suits:
+            if suit != trump:
+                marriage = [suit[0] + "K", suit[0] + "Q"]
+                self.scoreGroups(marriage, 20, 40)
+
+        print(self.getName() + "'s meld: " + str(self._meld))
+
 class Team:
 
     def __init__(self, player1, player2):
         self._p1 = player1
         self._p2 = player2
+
+    def getPartners(self):
+        return [self._p1, self._p2]
+
+    def getPartner1(self):
+        return self._p1
+
+    def getPartner2(self):
+        return self._p2
 
 class Pinochle:
 
@@ -104,6 +185,14 @@ class Pinochle:
         self._team2 = Team(self._p3, self._p4)
         self._deck = Deck()
         self._deck.shuffle()
+        self._trump = False
+        self._finalBid = False
+        self._tookBid = False
+        print(self._p1.getName(), self._p1)
+        print(self._p2.getName(), self._p2)
+        print(self._p3.getName(), self._p3)
+        print(self._p4.getName(), self._p4)
+
 
     def deal(self):
         for index in range(12):
@@ -113,6 +202,7 @@ class Pinochle:
     def getPlayer(self, playerName):
         for player in self._allPlayers:
             if player.getName() == playerName:
+                print("in loop: ", player)
                 return player
 
     def showHand(self, playerName):
@@ -123,7 +213,7 @@ class Pinochle:
     def bidding(self):
         passed = 0
         currentBid = 240
-        biddingPlayers = self._allPlayers
+        biddingPlayers = self._allPlayers.copy()
         first = True
         while passed < 3:
             for player in biddingPlayers.copy():
@@ -147,7 +237,52 @@ class Pinochle:
         if finalBid == "Pass":
             print("No bids, reshuffle?")
         else:
+            self._finalBid = finalBid
+            self._tookBid = biddingPlayers[0]
             print("Final bid: " + str(finalBid) + ", " + biddingPlayers[0].getName())
+            while True:
+                self._trump = input(biddingPlayers[0].getName() + ", enter trump suit:")
+                if self._trump in self._deck.getSuits():
+                    break
+
+    def passCards(self):
+        player = self._tookBid
+        if player in self._team1.getPartners():
+             if player == self._team1.getPartner1():
+                 partner = self._team1.getPartner2()
+             else:
+                partner = self._team1.getPartner1()
+        else:
+            if player == self._team2.getPartner1():
+                partner = self._team2.getPartner2()
+            else:
+                partner = self._team2.getPartner1()
+
+        passing = []
+        for i in range(4):
+            passing.append(input(partner.getName() + ", pass cards:"))
+
+        print(passing)
+        for card in passing:
+            player.fillHand(partner.getCardFromSimple(card))
+
+        for card in passing:
+            partner.removeCard(partner.getCardFromSimple(card))
+
+        passingBack = []
+        for i in range(4):
+            passingBack.append(input(player.getName() + ", pass four cards:"))
+
+        for card in passingBack:
+            partner.fillHand(player.getCardFromSimple(card))
+
+        for card in passingBack:
+            player.removeCard(player.getCardFromSimple(card))
+
+
+    def scoreMeld(self):
+        for player in self._allPlayers:
+            player.scoreHand(self._trump)
 
 
 if __name__ == '__main__':
@@ -156,4 +291,7 @@ if __name__ == '__main__':
     myGame.deal()
     myGame.showHand("Lizzy")
     myGame.bidding()
+    myGame.showHand("Micah")
+    myGame.passCards()
+    myGame.scoreMeld()
 
